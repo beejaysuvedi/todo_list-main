09dfb34ff04f0add5f3cfcec3676017d842ddfdc
Bijaya Subedi <beejaysuvedi@gmail.com>
2026-09-23 09:28:14 +0545

    Refactor AuthenticationProvider to manage loading state and return success status for signUp and signIn methods

--- lib/provider/authentication_provider.dart
+++ lib/provider/authentication_provider.dart
@@ -5,6 +5,8 @@ import 'package:todo_list/services/authentication_services.dart';
 class AuthenticationProvider extends ChangeNotifier{
   User? _user;
   User? get user => _user;
+  bool _isLoaded = false;
+  bool get isLoaded =>_isLoaded;
 
   Authenticationservices _authenticationservices = Authenticationservices();
 
@@ -12,13 +14,27 @@ class AuthenticationProvider extends ChangeNotifier{
     _user = _authenticationservices.currentUser;
   }
 
-  Future<void> signUp(String email, String password, String userName)async{
+  Future<bool> signUp(String email, String password, String userName)async{
+    try{
+      _isLoaded = true;
+      notifyListeners();
+
     _user =await _authenticationservices.signUp(email, password, userName);
+    _isLoaded = false;
     notifyListeners();
+    }catch(e){
+      _isLoaded = false;
+      notifyListeners();
+      return false;
+    }
+
+    return true;
   }
-   Future<void> signIn(String email, String password)async{
+   Future<bool> signIn(String email, String password)async{
     _user =await _authenticationservices.signIn(email, password);
+    _isLoaded = false;
     notifyListeners();
+    return true;
   }
   Future<void>signOut()async{
     await _authenticationservices.signOut();
