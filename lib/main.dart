a956ad8d63c8740806bceb565aafceb0d3ff0fd6
Bijaya Subedi <beejaysuvedi@gmail.com>
2026-09-23 09:28:04 +0545

    Refactor main.dart to initialize authentication provider and set AuthWrapperScreen as home

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,22 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_list/firebase_options.dart';
+import 'package:todo_list/provider/authentication_provider.dart';
 import 'package:todo_list/provider/image_picker_provider.dart';
 import 'package:todo_list/provider/theme_provider.dart';
 import 'package:todo_list/provider/todo_provider.dart';
+import 'package:todo_list/screens/auth_wrapper_screen.dart';
 import 'package:todo_list/screens/image_picker_screen.dart';
+import 'package:todo_list/screens/signin_screen.dart';
+import 'package:todo_list/screens/signup_screen.dart';
 import 'package:todo_list/screens/todo_screen.dart';
 
 void main()async {
+ WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
  options: DefaultFirebaseOptions.currentPlatform,
  );
- WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -26,6 +31,7 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => ThemeProvider()),
         ChangeNotifierProvider(create: (_) => TodoProvider()),
         ChangeNotifierProvider(create: (_)=>ImagePickerProvider()),
+        ChangeNotifierProvider(create: (_)=>AuthenticationProvider())
       ],
       child: Consumer<ThemeProvider>(
         builder: (context, themeProvider, _) {
@@ -43,7 +49,7 @@ class MyApp extends StatelessWidget {
               primarySwatch: Colors.indigo,
             ),
             themeMode: themeProvider.themeMode,
-            home: ImagePickerScreen(),
+            home: AuthWrapperScreen(),
           );
         },
       ),
