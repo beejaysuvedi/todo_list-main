84a2be5658af735123ca8c2983486860fe12ae6d
Bijaya Subedi <beejaysuvedi@gmail.com>
2026-09-23 09:28:38 +0545

    Enhance CreateAccountScreen with user input validation and feedback for sign-up process

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,5 +1,7 @@
-
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_list/provider/authentication_provider.dart';
+import 'package:todo_list/screens/todo_screen.dart';
 
 class CreateAccountScreen extends StatefulWidget {
   const CreateAccountScreen({super.key});
@@ -15,97 +17,116 @@ class _CreateAccountScreenState extends State<CreateAccountScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final authenticationProvider = Provider.of<AuthenticationProvider>(context);
     return Scaffold(
       backgroundColor: Color(0xffFFFFFF),
-      
+
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal:20),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
         child: SingleChildScrollView(
           child: Column(
-            
             crossAxisAlignment: CrossAxisAlignment.start,
-            
+
             children: [
-              SizedBox(height: 90,),
+              SizedBox(height: 90),
               CircleAvatar(
                 radius: 10,
                 backgroundColor: Colors.white,
-                child: InkWell(
-                 
-                  
-                  child: Icon(Icons.arrow_back_ios)),
+                child: InkWell(child: Icon(Icons.arrow_back_ios)),
               ),
-              SizedBox(height:30 ,),
-              Text("Create Account", style: TextStyle(fontSize: 32, fontWeight: FontWeight.w700),),
-          
-              SizedBox(height: 20,),
-          
-              
+              SizedBox(height: 30),
+              Text(
+                "Create Account",
+                style: TextStyle(fontSize: 32, fontWeight: FontWeight.w700),
+              ),
+
+              SizedBox(height: 20),
+
               TextField(
-          
+                controller: _userNameController,
+
                 decoration: InputDecoration(
-          
                   filled: true,
                   fillColor: Colors.grey.withValues(alpha: 0.15),
-                  hintText: "FirstName",
+                  hintText: "userName",
                   border: OutlineInputBorder(
                     borderSide: BorderSide.none,
-                    borderRadius: BorderRadius.circular(15)
-          
-                  )
+                    borderRadius: BorderRadius.circular(15),
+                  ),
                 ),
               ),
-              SizedBox(height: 20,),
-               
+
+              SizedBox(height: 20),
+
               TextField(
-          
+                controller: _emailController,
+
                 decoration: InputDecoration(
-          
-                  filled: true,
-                  fillColor: Colors.grey.withValues(alpha: 0.15),
-                  hintText: "LastName",
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide.none,
-                    borderRadius: BorderRadius.circular(15)
-          
-                  )
-                ),
-              ),
-              SizedBox(height: 20,),
-               
-              TextField(
-          
-                decoration: InputDecoration(
-          
                   filled: true,
                   fillColor: Colors.grey.withValues(alpha: 0.15),
                   hintText: "EmailAddress",
                   border: OutlineInputBorder(
                     borderSide: BorderSide.none,
-                    borderRadius: BorderRadius.circular(15)
-          
-                  )
+                    borderRadius: BorderRadius.circular(15),
+                  ),
                 ),
               ),
-              SizedBox(height: 20,),
-               
+              SizedBox(height: 20),
+
               TextField(
-          
+                controller: _passwordController,
+
                 decoration: InputDecoration(
-          
                   filled: true,
                   fillColor: Colors.grey.withValues(alpha: 0.15),
                   hintText: "Password",
                   border: OutlineInputBorder(
                     borderSide: BorderSide.none,
-                    borderRadius: BorderRadius.circular(15)
-          
-                  )
+                    borderRadius: BorderRadius.circular(15),
+                  ),
                 ),
               ),
-              SizedBox(height: 50,),
+              SizedBox(height: 50),
               InkWell(
-                
+                onTap: () async {
+                  final userName = _userNameController.text;
+                  final email = _emailController.text;
+                  final password = _passwordController.text;
+
+                  if (!email.endsWith("@gmail.com")) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text("Invalid email"),
+                        backgroundColor: Colors.red,
+                      ),
+                    );
+                    return;
+                  }
+                  final result = await authenticationProvider.signUp(
+                    email,
+                    password,
+                    userName,
+                  );
+                  if (result) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text("Account Created Successfully"),
+                        backgroundColor: Colors.green,
+                      ),
+                    );
+                    Navigator.pop(context);
+
+
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text("Failed to Create account"),
+                        backgroundColor: Colors.red,
+                      ),
+                    );
+                  }
+                },
+
                 child: Container(
                   height: 60,
                   width: double.maxFinite,
@@ -113,40 +134,38 @@ class _CreateAccountScreenState extends State<CreateAccountScreen> {
                     color: Color(0xff8E6CEF),
                     borderRadius: BorderRadius.circular(20),
                   ),
-                  child: Center(child: Text("Continue", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400, color: Color(0xffFFFFFF)),)),
-                        
-                          
+                  child: authenticationProvider.isLoaded
+                      ? Center(
+                          child: CircularProgressIndicator(color: Colors.white),
+                        )
+                      : Center(
+                          child: Text(
+                            "Continue",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                              color: Color(0xffFFFFFF),
+                            ),
+                          ),
+                        ), 
                 ),
               ),
-              SizedBox(height: 20,),
-                RichText(
-                
-                text:TextSpan(
+              SizedBox(height: 20),
+              RichText(
+                text: TextSpan(
                   text: "Forget Password?",
                   children: [
                     TextSpan(
-                      text: "Reset", style: TextStyle(fontWeight: FontWeight.w600)
-                    )
-                  ]
-                ) ),
-          
-
-             
-             
-              
-          
-              
-          
+                      text: "Reset",
+                      style: TextStyle(fontWeight: FontWeight.w600),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
       ),
-      
-
-
-
-
-
     );
   }
 }
