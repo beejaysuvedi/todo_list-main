a3e97bf073a3abf02ff49a6554777e4bf2eea5e7
Bijaya Subedi <beejaysuvedi@gmail.com>
2026-09-24 09:15:51 +0545

    Add PersonServices class for managing person data in Firestore

---
+++ lib/services/person_services.dart
@@ -0,0 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:todo_list/model/person_model.dart';
+
+class PersonServices {
+  final _collection = FirebaseFirestore.instance.collection('persons');
+
+  Future<void> addPerson(PersonModel person) async {
+    await _collection.doc(person.id).set(person.toJson());
+  }
+  Future<List<PersonModel>>fetchPersons()async{
+    final snapshot = await _collection.get();
+    return snapshot.docs.map((item) => PersonModel.fromJson(item.data())).toList();
+  }
+  Future<void> updatePerson(PersonModel person) async {
+    await _collection.doc(person.id).update(person.toJson());
+  }
+  Future<void> deletePerson(String id) async {
+    await _collection.doc(id).delete();
+  }
+  
+}
